61d370aabe6bc36fd2d23c9f357e520d6011b8c0
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2025-05-01 21:41:05 -0700

    add input

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Camera, Search } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex justify-center p-5">
-      <Button variant='secondary'>
-      Hello
-    </Button>
+    <div className="h-screen w-full flex flex-col items-center p-5 gap-2">
+      <div className="flex h-24 relative items-center w-9/10 md:w-auto">
+        <Input
+          placeholder="Search for Items..."
+          className="max-w-screen w-120 rounded-full p-6"
+        />
+        <Button variant="primary" className="rounded-full absolute right-2 cursor-pointer">
+          <Search size={6} />
+        </Button>
+        <Button variant="outline" className="rounded-full absolute right-13 cursor-pointer">
+          <Camera size={6}/>
+        </Button>
+      </div>
     </div>
   );
 }
